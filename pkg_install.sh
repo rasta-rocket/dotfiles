dbc60ad0d16eadd8ed1eaacef5c6e31c7ab46c88
rasta-rocket <bruno.fernandos@yahoo.com>
2016-08-22 15:38:27 +0200

    Fix some issues with root permissions

--- pkg_install.sh
+++ pkg_install.sh
@@ -24,11 +24,11 @@ DEPENDENCIES="exuberant-ctags"
 UTILITIES="tree curl"
 
 echo "Install dependencies: Begin"
-apt-get -y install $DEPENDENCIES
+sudo apt-get -y install $DEPENDENCIES
 echo "Install dependencies: End"
  
 echo "Install utilities: Begin"
-apt-get -y install $UTILITIES
+sudo apt-get -y install $UTILITIES
 echo "Install utilities: End"
 
 echo "Install bash prompt"
